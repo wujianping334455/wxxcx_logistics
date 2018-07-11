c5c6ad671f1edc70a84c573cce01769485cb2295
wujp <wujianping@ctirobot.com>
2018-07-11 18:19:23 +0800

    删除不需要的组件

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ export default {
   // 这个字段走 app.json
   config: {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: ['pages/logs/main','^pages/home/main','pages/order/main', 'pages/my/main','pages/index/main','pages/pay/main'],
+    pages: ['^pages/home/main','pages/order/main','pages/my/main','pages/index/main'],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
