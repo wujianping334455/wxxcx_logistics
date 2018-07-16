ac712f2c0fce2ab6c2fc5e085cdcf26333c086f0
wujp <wujianping@ctirobot.com>
2018-07-16 18:45:12 +0800

    我的

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ export default {
   // 这个字段走 app.json
   config: {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: ['^pages/home/main','pages/order/main','pages/my/main','pages/index/main'],
+    pages: ['^pages/home/main','pages/order/main','pages/user/main','pages/index/main'],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
@@ -49,7 +49,7 @@ export default {
           text: "订单"
         },
         {
-          pagePath: "pages/my/main",
+          pagePath: "pages/user/main",
           iconPath: "static/images/my.png",
           selectedIconPath: "static/images/my-selected.png",
           text: "我的"
